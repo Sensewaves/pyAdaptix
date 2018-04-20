6c36c01889adbb656f24a4d7f3ced04638bafd0c
Abderrahim OUAKKI <abderrahim@sensewaves.io>
2018-04-20 17:01:14 +0200

    reorganized the code to make a python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='py_adaptix',
+      version='0.1',
+      description='A python interface to the Adaptix platform API ',
+      url='https://github.com/Sensewaves/pyAdaptix',
+      author='Sensewaves',
+      author_email='admin@adaptix.io',
+      license='GNU',
+      install_requires=['requests'],
+      )
